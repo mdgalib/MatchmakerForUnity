5d9a811224842496e639d335cbce7fc2b4c5e3ed
fredfishgames <fred.miller097@gmail.com>
2017-01-29 15:02:12 +0000

    Added basic functionality to the server
    This reverts commit 01890422174c7037dc1a38706c3f05d091ad5883.
    Added ability to change hostname
    
    
    Added basic functionality to the server

--- Server/matchmaking-server-python/matchmaking_server_python.py
+++ Server/matchmaking-server-python/matchmaking_server_python.py
@@ -1,8 +1,11 @@
 import server
 
 def start():
+    startmessage = '''fredfishgames matchmaking server for unity
+Licenced under the MIT licence
+Make sure the configuration file has been set up before starting'''
     # Start the server here!
-    print('fredfishgames matchmaking server for unity.')
+    print(startmessage)
     s = server.server()
     s.start()
 start()
--- Server/matchmaking-server-python/server.py
+++ Server/matchmaking-server-python/server.py
@@ -1,5 +1,6 @@
 import socket
 import six.moves.configparser
+import _thread
 
 class server(object) :
     PORT = 25570
@@ -7,22 +8,65 @@ class server(object) :
     def start(self):
         self.CONFIG = six.moves.configparser.ConfigParser()
         self.CONFIG.read('settings.conf')
-        self.PORT = int(self.CONFIG.get('settings', 'port'))
-        self.GAME = self.CONFIG.get('settings', 'game')
+        self.SERVERS = []
+        try:
+            self.PORT = int(self.CONFIG.get('settings', 'port'))
+            self.GAME = self.CONFIG.get('settings', 'game')
+        except six.moves.configparser.NoOptionError as error:
+            print('Config file at settings.conf not configured correctly.')
+            print(error)
+            return
+        
         print('Starting {} matchmaking server on port {}'.format(self.GAME, self.PORT))
 
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversocket.bind((socket.gethostname(), 80))
+        self.serversocket.bind((socket.gethostname(), self.PORT))
         self.serversocket.listen(5)
 
         while 1:
-            update()
+            self.update()
 
     def update(self):
-        (clientsocket, address) = self.serversocket.accept
+        # Accepts a socket request
+        (clientsocket, address) = self.serversocket.accept()
+        # Creates a thread for the request to run on
+        self.t = _thread.start_new_thread(self.client_thread, (clientsocket, address, ))
 
-        thread = client_thread(clientsocket)
-        ct.run()
-
-    def client_thread(self, clientSocket):
-        print("Handling the client's request")
+    def client_thread(self, clientSocket, Address):
+        print("Player connected from {}".format(Address[0]))
+        request = clientSocket.recv(1024).decode('utf-8').split(';')
+        print('Recieved request {}'.format(request))
+        if(request[0] == 'send'):
+            # This request is when the client wants to register their game
+            self.SERVERS.append(Address[0])
+            print('Added address {} to the index'.format(Address[0]))
+            clientSocket.send(b'done')
+        elif(request[0] == 'get'):
+            if(len(self.SERVERS) == 0):
+                # This means that there are no servers on the list
+                print('No servers...')
+                clientSocket.send(b'none')
+            else:
+                # This means there are servers on the list
+                # Sends back the address of the first server on the list
+                clientSocket.send(self.SERVERS[0].encode('utf-8'))
+        elif(request[0] == 'full'):
+            # This means that the server should be removed from the list
+            if(len(self.SERVERS)>0):
+                if(request[1] == '\r\n'):
+                    adrs = Address[0]
+                else:
+                    adrs = request[1]
+                index = 0
+                deleted = 0
+                while(index < len(self.SERVERS)):
+                    if(self.SERVERS[index] == adrs):
+                        deleted += 1
+                        self.SERVERS.pop(index)
+                        index -= 1
+                    index += 1
+                print('cleared {} occurences of {}'.format(deleted, adrs))
+        elif(request[0] == 'status'):
+            clientSocket.send(b'yes')
+        clientSocket.close()
+        
